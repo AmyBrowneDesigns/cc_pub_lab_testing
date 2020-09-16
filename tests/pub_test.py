a16521f84a5ab5600f0569b1e8fd8e120dcfbe74
AmyBrowneDesigns <amybrowne20@gmail.com>
2020-09-16 11:59:57 +0100

    initial commit

---
+++ tests/pub_test.py
@@ -0,0 +1,13 @@
+import unittest
+from src.pub import Pub 
+
+class TestPub(unittest.TestCase):
+    def setUp(self):
+        self.pub = Pub("Ox", 100.00)
+
+    def test_pub_has_name(self):
+        self.assertEqual("Ox", self.pub.name)
+
+
+    def 
+    
